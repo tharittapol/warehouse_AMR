07cd5cda0f6bb696e57ece975e441f7227fda8dd
tharittapol <tharitpol.big@gmail.com>
2025-07-31 00:17:19 +0700

    add wrapper slam toolbox and maps

--- src/slam/setup.py
+++ src/slam/setup.py
@@ -1,8 +1,6 @@
-import os
-from glob import glob
 from setuptools import find_packages, setup
 
-package_name = 'gazebo_sim'
+package_name = 'slam'
 
 setup(
     name=package_name,
@@ -11,16 +9,15 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
+        ('share/' + package_name + '/launch', ['launch/slam_toolbox.launch.py']),
+        ('share/' + package_name + '/config', ['config/slam_toolbox_params.yaml']),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.*'))),
-        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.*'))),
-        (os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', '*.*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Tharittapol Takhumsuk',
     maintainer_email='tharitpol.big@gmail.com',
-    description='gazebo simulation package for Jetrover',
+    description='SLAM Toolbox Launch',
     license='MIT',
     tests_require=['pytest'],
     entry_points={
